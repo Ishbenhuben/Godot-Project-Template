043bea65b7354db19c138dbd4b429df92b6d4376
Ishbenhuben <gmarksantos@gmail.com>
2024-10-24 00:59:49 +0800

    added PlayerData

---
+++ Autoloads/PlayerData.gd
@@ -0,0 +1,3 @@
+extends Node
+
+# load player data here
--- Screens/main_menu_screen.gd
+++ Screens/main_menu_screen.gd
@@ -1,5 +1,4 @@
 extends Screen
 
-
 func _on_tutorial_button_up() -> void:
 	ScreenManager.go_to_screen("res://Screens/Tests/TestScreen.tscn")
